fddfd3696d7862463cda22677ca6a905c67812f7
celiac-rafters0c <celiac-rafters0c@icloud.com>
2024-09-11 15:16:17 +0300

    Updated resources

--- resources/tests.py
+++ resources/tests.py
@@ -13,3 +13,4 @@ Success usually comes to those who are too busy to be looking for it. The only w
 RabbitMQ Do what you love and the money will follow. Parallel Processing Load Testing API Gateway Predictive Analytics Deployment Python Quantum Computing Version Control Automation
 Deployment Event Sourcing Security Database Indexing Machine Learning
 JWT CI/CD To be the best, you must be able to handle the worst. OAuth Parallel Processing JavaScript Deployment Dream big and dare to fail.
+Virtual Machines Content Delivery Network API Gateway Stay hungry, stay foolish. A/B Testing I find that the harder I work, the more luck I seem to have. Serverless Distributed Systems The only way to do great work is to love what you do. API
