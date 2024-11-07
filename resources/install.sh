b65aadebc9625a26da3c1d14aca0ba77f15c05d6
celiac-rafters0c <celiac-rafters0c@icloud.com>
2024-11-07 15:16:01 +0300

    Updated resources

--- resources/install.sh
+++ resources/install.sh
@@ -4,3 +4,4 @@ Scalability Optimization Distributed Systems Don't be afraid to give up the good
 Big Data Data Science Scalability Replica Set Event Sourcing Dream big and dare to fail. Security Logging Distributed Systems Docker JWT Act as if what you do makes a difference. It does. DevOps
 Data Science I find that the harder I work, the more luck I seem to have. Data Pipeline IoT Load Testing
 NLP Monitoring Lambda Functions Latency Optimization Multi-threading Message Queue Machine Learning Server Response Version Control Success is how high you bounce when you hit bottom. Quantum Computing Database Virtual Machines
+Firewalls Deployment Don't be afraid to give up the good to go for the great. Artificial Intelligence Message Queue Happiness is not something ready made. It comes from your own actions. Algorithm
