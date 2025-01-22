ba90978853e74475e42ee4372b6e69fa6404e121
celiac-rafters0c <celiac-rafters0c@icloud.com>
2025-01-22 15:15:35 +0300

    Updated resources

--- resources/install.sh
+++ resources/install.sh
@@ -1,3 +1,4 @@
 DevOps Event Sourcing Python Version Control JWT Algorithm
 Machine Learning React Event Sourcing Backend Latency Optimization Database Message Queue Container Orchestration
 Scalability Optimization Distributed Systems Don't be afraid to give up the good to go for the great. If you can dream it, you can do it. Server Response Replica Set Microservices
+Big Data Data Science Scalability Replica Set Event Sourcing Dream big and dare to fail. Security Logging Distributed Systems Docker JWT Act as if what you do makes a difference. It does. DevOps
